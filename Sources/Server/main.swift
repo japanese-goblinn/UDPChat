e03480062951ce6a79c12592a5a75cd55382079a
japanese-goblinn <cool45akol@gmail.com>
2020-03-05 00:54:17 +0300

    FEAT: implement naive server

--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -1,3 +1,30 @@
-import Foundation
+import CocoaAsyncSocket
 
-print("Hello Server")
+class Server: NSObject, GCDAsyncUdpSocketDelegate {
+    
+    var socket: GCDAsyncUdpSocket!
+    let inernalQueue = DispatchQueue(label: "com.japanese-goblinn.server")
+    
+    override init() {
+        super.init()
+        socket = GCDAsyncUdpSocket(delegate: self, delegateQueue: inernalQueue)
+        do {
+            try socket.enableReusePort(true)
+            try socket.bind(toPort: 5000)
+            try socket.beginReceiving()
+        } catch {
+            print(error.localizedDescription)
+            exit(EXIT_FAILURE)
+        }
+    }
+    
+    func udpSocket(_ sock: GCDAsyncUdpSocket, didReceive data: Data, fromAddress address: Data, withFilterContext filterContext: Any?) {
+        print(String(decoding: data, as: UTF8.self))
+    }
+}
+
+let server = Server()
+print("✨ Server is running... ✨")
+while true {
+    sleep(20)
+}
