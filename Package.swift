534d2467698e48d8eade894a6beb3f4ee6739bb6
japanese-goblinn <cool45akol@gmail.com>
2020-03-09 11:01:11 +0300

    FIX: update package content

--- Package.swift
+++ Package.swift
@@ -6,14 +6,16 @@ import PackageDescription
 let package = Package(
     name: "UDPChat",
     dependencies: [
-        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4")
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.0.1")
     ],
     targets: [
+        .target(name: "UDPCore"),
         .target(
             name: "Client",
-            dependencies: ["CocoaAsyncSocket"]),
+            dependencies: ["UDPCore", "CocoaAsyncSocket", "SwiftToolsSupport"]),
         .target(
             name: "Server",
-            dependencies: ["CocoaAsyncSocket"])
+            dependencies: ["UDPCore", "CocoaAsyncSocket"])
     ]
 )
