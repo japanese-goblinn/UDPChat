d41b51b91b5b2d2ab54668c6a02a5bc30e3f47a1
japanese-goblinn <cool45akol@gmail.com>
2020-03-09 11:03:55 +0300

    FEAT: implement Server

--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -1,30 +1,6 @@
-import CocoaAsyncSocket
-
-class Server: NSObject, GCDAsyncUdpSocketDelegate {
-    
-    var socket: GCDAsyncUdpSocket!
-    let inernalQueue = DispatchQueue(label: "com.japanese-goblinn.server")
-    
-    override init() {
-        super.init()
-        socket = GCDAsyncUdpSocket(delegate: self, delegateQueue: inernalQueue)
-        do {
-            try socket.enableReusePort(true)
-            try socket.bind(toPort: 5000)
-            try socket.beginReceiving()
-        } catch {
-            print(error.localizedDescription)
-            exit(EXIT_FAILURE)
-        }
-    }
-    
-    func udpSocket(_ sock: GCDAsyncUdpSocket, didReceive data: Data, fromAddress address: Data, withFilterContext filterContext: Any?) {
-        print(String(decoding: data, as: UTF8.self))
-    }
-}
+import Foundation
+import UDPCore
 
 let server = Server()
 print("✨ Server is running... ✨")
-while true {
-    sleep(20)
-}
+RunLoop.current.run()
